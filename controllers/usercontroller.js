ff922862f733b99935eb7867dcf137a2aba33722
sumeetmishra1 <sumitsfs0@gmail.com>
2023-12-01 15:10:48 +0530

    encryption

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,38 +1,54 @@
 const User=require('../models/newuser');
-
+const bcrypt=require('bcrypt');
 exports.addNewUser = async(req,res,next)=>{
     const name=req.body.name;
     const email=req.body.email;
     const password=req.body.password;
     const existinguser= await User.findAll({where:{email:email}});
+    try{
     if(existinguser.length>0){
         res.status(400).json({err:"User already exists!"});
         console.log("user already exists")
     }
     else{
-        const user=await User.create({
-            name:name,
-            email:email,
-            password:password
-        })
-        res.status(201).json({newUser:user});
+        const saltrounds=10;
+        bcrypt.hash(password,saltrounds,async(err,hash)=>{
+            const user=await User.create({
+                name:name,
+                email:email,
+                password:hash
+            })
+            res.status(201).json({newUser:user});
+        })   
     }
-
+    } 
+    catch(e){
+        res.status(500).json(err);
+    }  
 }
 exports.loginUser=async(req,res,next)=>{
     const email=req.body.email;
     const password=req.body.password;
+    try{
     const user=await User.findAll({where:{email:email}});
     if(user.length>0){
-        if(user[0].password===password){
-            res.status(200).json({message:"User Login Sucessfull!"});
-        }
-        else{
-            res.status(401).json({err:"User not authorized"});
-        }
+        bcrypt.compare(password,user[0].password,(err,result)=>{
+            if(err){
+                res.status(500).json({message:"Something went Wrong"});
+            }
+            if(result==true){
+                res.status(200).json({message:"User Login Sucessfull!"});
+            }
+            else{
+                res.status(401).json({message:"Password is incorrect"});
+            }
+        })
     }
     else{
-        res.status(404).json({err:"User Not Found"});
+        res.status(404).json({message:"User Not Found"});
     }
-
+}
+catch(e){
+    res.status(500).json({message:e})
+}
 }
