dbf3f5a86798d70f6b95619db91a26951fe6e128
sumeetmishra1 <sumitsfs0@gmail.com>
2023-12-04 14:49:55 +0530

    leaderboard optimization

--- controllers/premiumcontroller.js
+++ controllers/premiumcontroller.js
@@ -1,17 +1,18 @@
 const Expenses=require('../models/expenses');
-
+const User=require("../models/newuser");
+const sequelize = require('../utils/database');
 
 exports.getleaderboard=async(req,res,next)=>{
-    const expense=await Expenses.findAll();
-    let map1=new Map();
-    for(var i=0;i<expense.length;i++){
-        if(map1.has(expense[i].userId)){
-            let amt=map1.get(expense[i].userId)+expense[i].amount;
-            map1.set(expense[i].userId,amt);
-        }
-        else{
-        map1.set(expense[i].userId,expense[i].amount);
-        }
-    }
-    res.status(200).json({leaderboard:map1});
+    const leaderboard=await User.findAll({
+        attributes:['id','name',[sequelize.fn('sum',sequelize.col('Expenses.amount')),'total_cost']],
+        include:[
+            {
+                model:Expenses,
+                attributes:[]
+            }
+        ],
+        group:['User.id'],
+        order:[['total_cost','DESC']]
+    });
+    res.status(200).json({leaderboard:leaderboard});
 }
