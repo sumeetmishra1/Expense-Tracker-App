8f05dca993ac7d106652d9ab3a7d64da66ee02ea
sumeetmishra1 <sumitsfs0@gmail.com>
2024-02-01 12:51:35 +0530

    test

--- routes/expenseroutes.js
+++ routes/expenseroutes.js
@@ -1,11 +1,16 @@
 const express=require('express');
 const router=express.Router();
 const Authoriz=require('../middleware/auth');
-const expensecontroller=require('../controllers/expensecontroller');
+const expensecontroller=require('../controllers/expensecontroller'); 
+
 router.post('/addexpense',Authoriz.authenticate,expensecontroller.addexpense);
+
  router.get('/getexpense',Authoriz.authenticate,expensecontroller.getexpense);
+
  router.get('/download',Authoriz.authenticate,expensecontroller.downloadexpense);
+
  router.get('/get-downloads',Authoriz.authenticate,expensecontroller.getlistofdownloads);
+ 
  router.delete('/delete/:id',Authoriz.authenticate,expensecontroller.deleteexpense);
 
 module.exports=router;
