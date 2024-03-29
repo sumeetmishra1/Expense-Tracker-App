3dab1b2703681a321ae148028f22fcd92f722701
sumeetmishra1 <sumitsfs0@gmail.com>
2024-03-29 16:38:01 +0530

    Integrated Razorpay

--- Expense-Tracker-Frontend/src/components/ExpensePage/RazorpayPage/index.js
+++ Expense-Tracker-Frontend/src/components/ExpensePage/RazorpayPage/index.js
@@ -1,21 +1,19 @@
 import axios from "axios"
-
-
-export default async function paymentPage(){
+export default async function paymentPage(cb){
         console.log("clicked")
-        const token=localStorage.getItem('token');
-        const response= await axios.get('/purchase/premiummembership',{headers:{"Authorization":token}})
+        const token =  localStorage.getItem('auth_token');
+        const response = await axios.get('http://localhost:8006/purchase/premiummembership',{headers:{"Authorization":token}})
         console.log(response);
         var options=
         {
             "key":response.data.key_id,
             "order_id":response.data.order.id,
             "handler":async function(response){
-                await axios.post('/purchase/updatetransactionstatus',{
+                await axios.post('http://localhost:8006/purchase/updatetransactionstatus',{
                     order_id:options.order_id,
                     payment_id:response.razorpay_payment_id,
                 },{headers:{"Authorization":token}})
-    
+                cb(null)
                 alert("You are a premium User");
             }
         }
@@ -23,6 +21,8 @@ export default async function paymentPage(){
         rzp1.open();
         rzp1.on('payment.failed',function(response){
             console.log(response);
+            var isError = "Something Went Wrong"
+            cb(isError)
             alert("Something Went Wrong");
         })
 }
